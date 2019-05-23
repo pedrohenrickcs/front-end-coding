a6578904d4c9e43dac648754bf282305e098d305
unknown <pedro.henrickcs@gmail.com>
2019-05-22 22:40:22 -0300

    transform in class

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Search = ( props ) => {
-
-    const { item } = props;
-
-    console.log('item search ====', item);
+class Search extends Component {
+    constructor(props) {
+        super(props)
+        console.log('item search ====', props);
+    }
     
-
-    const handleInputChange = event => {
-        const query = event.target.value;
-
-        this.setState(prevState => {
-            const filter = prevState.item.filter(element => {
-                console.log('ELEMENT IS', element.name.toLowerCase().includes(query.toLowerCase()));
+    handleInputChange(event) {
+        // const query = event.target.value;
+        console.log('e', this);
+        
+    
+        // this.setState(prevState => {
+        //     const filter = prevState.item.filter(element => {
+        //         console.log('ELEMENT IS', element.name.toLowerCase().includes(query.toLowerCase()));
                 
-                return element.name.toLowerCase().includes(query.toLowerCase());
-            })
-
-            return {
-                filter
-            };
-        })
+        //         return element.name.toLowerCase().includes(query.toLowerCase());
+        //     })
+    
+        //     return {
+        //         filter
+        //     };
+        // })
     }
 
-    return(
-        <form className="search">
-            <input id="venueType" placeholder="Pesquisar" onChange={handleInputChange} />
-            <input type="submit" value="Submit" />
-        </form>
-    )
+    componentDidMount() {
+        this.handleInputChange()
+    }
+
+    render() {
+
+        return(
+            <form className="search">
+                <input id="venueType" placeholder="Pesquisar" onChange={this.handleInputChange} />
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+    
 }
+
+
 
 export default Search;
